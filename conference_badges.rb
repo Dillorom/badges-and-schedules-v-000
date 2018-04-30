85190e2e6723a44d5f10dcc62b168cdf1d51c94a
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-30 16:23:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+#name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendee)
+  return badge_maker(name)
+end
