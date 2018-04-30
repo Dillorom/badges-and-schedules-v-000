83843ca5927b9a99be3cc157ca98ee2f5f24b057
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-30 16:44:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
-  batch_badge_creator(name) == badge_maker(name)
-
+def batch_badge_creator(attendees)
   attendees.each do |attendee|
     return  "Hello, my name is #{attendee}."
     counter = counter+1
